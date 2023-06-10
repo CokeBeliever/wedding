bc3bafef20f109d7fff37945e31ae38d0f2c2895
CokeBeliever <cokebeliever@163.com>
2023-06-10 23:13:31 +0800

    编写 e2e test

--- service/test/app.e2e-spec.ts
+++ service/test/app.e2e-spec.ts
@@ -1,24 +1,127 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from 'src/app.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SignUpDto } from 'src/auth/dto';
+import { ADMIN_EMAIL, ADMIN_PASSWORD } from './utils/constants';
+import { EditUserDto } from 'src/user/dto';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
+  let httpServer: any;
+  let admin: any;
+  let token: string;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+
+    const prismaService: PrismaService = app.get(PrismaService);
+    await prismaService.clearDb();
+
+    httpServer = app.getHttpServer();
+
+    const signUpDto: SignUpDto = {
+      email: ADMIN_EMAIL,
+      password: ADMIN_PASSWORD,
+    };
+    await request(httpServer)
+      .post('/auth/sign-up')
+      .send(signUpDto)
+      .expect(201)
+      .expect(({ body }) => {
+        expect(body.user).toBeDefined();
+        expect(body.token).toBeDefined();
+        admin = body.user;
+        token = body.token;
+      });
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Auth Controller', () => {
+    const signUpDto: SignUpDto = {
+      email: 'cokebeliever@163.com',
+      password: 'helloworld',
+    };
+
+    describe('signUp', () => {
+      it('应该注册成功', () => {
+        return request(httpServer)
+          .post('/auth/sign-up')
+          .send(signUpDto)
+          .expect(201)
+          .expect(({ body }) => {
+            expect(body.user).toBeDefined();
+            expect(body.token).toBeDefined();
+          });
+      });
+
+      it('应该注册失败，用户已注册', () => {
+        return request(httpServer)
+          .post('/auth/sign-up')
+          .send(signUpDto)
+          .expect(403);
+      });
+    });
+
+    describe('signIn', () => {
+      it('应该登陆成功', () => {
+        return request(httpServer)
+          .post('/auth/sign-in')
+          .send(signUpDto)
+          .expect(200)
+          .expect(({ body }) => {
+            expect(body.user).toBeDefined();
+            expect(body.token).toBeDefined();
+          });
+      });
+
+      it('应该登陆失败，凭证不正确', () => {
+        const unRegisteredUser: SignUpDto = {
+          email: 'foo@163.com',
+          password: '123456',
+        };
+
+        return request(httpServer)
+          .post('/auth/sign-in')
+          .send(unRegisteredUser)
+          .expect(403);
+      });
+    });
+  });
+
+  describe('User Controller', () => {
+    describe('editById', () => {
+      const dto: EditUserDto = {
+        username: 'admin',
+        phoneNumber: '18012345678',
+      };
+      it('应该修改成功', () => {
+        console.log(token, admin);
+        return request(httpServer)
+          .put(`/user/${admin.userId}`)
+          .set('authorization', token)
+          .send(dto)
+          .expect(200)
+          .expect(({ body }) => {
+            expect(body).toBeDefined();
+            expect(body.username).toBe(dto.username);
+            expect(body.phoneNumber).toBe(dto.phoneNumber);
+          });
+      });
+    });
   });
 });
---
+++ service/test/utils/constants.ts
@@ -0,0 +1,3 @@
+export const APP_URL = process.env.APP_URL;
+export const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
