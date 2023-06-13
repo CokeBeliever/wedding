95bd8931905bca3fbb54c3c13143c10b2703fbe2
CokeBeliever <cokebeliever@163.com>
2023-06-13 22:26:51 +0800

    chore(client): 配置 vite server proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [uni()],
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [uni()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.SERVER_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  });
+};
