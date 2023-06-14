4947e67b5c2a1ead25b5a1bb7f22972b38ad92fa
CokeBeliever <cokebeliever@163.com>
2023-06-14 22:35:59 +0800

    fix(client): 修复 pinia 配置可能存在的问题

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,14 @@
 import { createSSRApp } from "vue";
-import { createPinia } from "pinia";
+import * as Pinia from "pinia";
 import App from "./App.vue";
 
 export function createApp() {
-  const pinia = createPinia();
   const app = createSSRApp(App);
 
-  app.use(pinia);
+  app.use(Pinia.createPinia());
 
   return {
     app,
+    Pinia,
   };
 }
