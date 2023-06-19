2e2d56947b639c759618271580ff2c9ef87a522f
CokeBeliever <cokebeliever@163.com>
2023-06-20 00:09:39 +0800

    style(client): 完善首页样式

--- client/src/pages/home/components/theme-filter.vue
+++ client/src/pages/home/components/theme-filter.vue
@@ -1,61 +1,57 @@
 <template>
   <view class="theme-filter">
-    <view class="theme-select-list">
-      <uni-data-select
-        class="theme-select"
-        v-model="basicThemeSelectValue"
-        :localdata="basicThemeSelectLocaldata"
-        :clear="false"
-        @change="onChangeBasicSelect"
-      ></uni-data-select>
-      <uni-data-select
-        class="theme-select"
-        v-model="basicThemeSelectValue"
-        :localdata="basicThemeSelectLocaldata"
-        :clear="false"
-        @change="onChangeBasicSelect"
-      ></uni-data-select>
-      <uni-data-select
-        class="theme-select"
-        v-model="basicThemeSelectValue"
-        :localdata="basicThemeSelectLocaldata"
-        :clear="false"
-        @change="onChangeBasicSelect"
-      ></uni-data-select>
-    </view>
+    <uni-data-select
+      class="theme-filter__select"
+      v-model="filterSelectValue"
+      :localdata="filterSelectLocaldata"
+      :clear="false"
+      @change="onChangeFilterSelect"
+    ></uni-data-select>
+    <uni-search-bar
+      class="theme-filter__search"
+      v-model="filterSearchValue"
+      radius="100"
+      placeholder="搜索更多主题"
+      maxlength="10"
+      @confirm="onFilterSearchConfirm"
+    />
   </view>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
 
-let basicThemeSelectValue = ref("1");
-const basicThemeSelectLocaldata = [
+let filterSelectValue = ref("1");
+const filterSelectLocaldata = [
   { text: "最新", value: "1" },
   { text: "最热", value: "2" },
 ];
+const onChangeFilterSelect = (newVal: string) => {
+  filterSelectValue.value = newVal;
+};
 
-const onChangeBasicSelect = (newVal: string) => {
-  basicThemeSelectValue.value = newVal;
+const filterSearchValue = ref("");
+const onFilterSearchConfirm = () => {
+  uni.showToast({
+    title: "搜索：" + filterSearchValue.value,
+    icon: "none",
+  });
 };
 </script>
 
 <style lang="scss" scoped>
 .theme-filter {
-  box-sizing: border-box;
-  padding: 20rpx;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  padding: 0 20rpx;
 
-  .theme-select-list {
-    display: flex;
-    flex-flow: row nowrap;
+  .theme-filter__select {
+    flex: 1;
+  }
 
-    .theme-select {
-      flex: 1;
-
-      &:nth-child(n + 2) {
-        margin-left: 20rpx;
-      }
-    }
+  .theme-filter__search {
+    flex: 4;
   }
 }
 </style>
--- client/src/pages/home/index.vue
+++ client/src/pages/home/index.vue
@@ -1,13 +1,17 @@
 <template>
-  <theme-searcher />
+  <image class="home-banner" src="/static/home/banner.jpg" />
   <theme-filter />
   <theme-list />
 </template>
 
 <script lang="ts" setup>
-import ThemeSearcher from "./components/theme-searcher.vue";
 import ThemeFilter from "./components/theme-filter.vue";
 import ThemeList from "./components/theme-list.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home-banner {
+  width: 100%;
+  height: calc(44px + env(safe-area-inset-top) + var(--status-bar-height));
+}
+</style>
